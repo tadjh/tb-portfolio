ecdaac179ae39bc6d38830e1bf20a01b7e0ed941
TadjhBrooks <tadjh.brooks@gmail.com>
2022-07-13 16:50:23 -0700

    feat(components): Create Socials

---
+++ components/Socials/index.tsx
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { socials } from "../../config/socials";
+import { composeKey } from "../../utils";
+
+export default function Socials() {
+  return (
+    <div className="flex flex-row gap-x-4">
+      {socials.map(({ url, icon }, index) => (
+        <a
+          key={composeKey("social", index)}
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={icon} className="h-10 w-10" />
+        </a>
+      ))}
+    </div>
+  );
+}
---
+++ config/socials.ts
@@ -0,0 +1,7 @@
+import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { Socials } from "../types";
+
+export const socials: Socials[] = [
+  { url: "https://github.com/tadjh", icon: faGithub },
+  { url: "https://twitter.com/tadjh_", icon: faTwitter },
+];
