d7c759e5f87da4051456dc47c2d36f5933ab3d15
TadjhBrooks <tadjh.brooks@gmail.com>
2022-07-13 17:12:13 -0700

    Add siteTitle and jobTitle

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,6 @@
 export const config = {
+  siteTitle: "Tadjh Brooks",
+  jobTitle: "Software Engineer",
   bio: "Self-taught full-stack software developer with a voracious appetite for learning where I hope to apply my skills in developing, problem solving and user experience on real products that make a difference in people's lives.",
   url: { domain: "https://tadjh.dev", vanity: "tadjh.dev" },
 };
