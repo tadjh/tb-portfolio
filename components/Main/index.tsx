1d2e7e06296f5f2667faffbba2f158702114907f
TadjhBrooks <tadjh.brooks@gmail.com>
2022-07-13 17:17:27 -0700

    feat(components): Simplify Main styling

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,9 +1,5 @@
 import { HTMLAttributes } from "react";
 
 export default function Main({ children }: HTMLAttributes<HTMLElement>) {
-  return (
-    <main className="flex flex-1 flex-col justify-around gap-y-10 px-4">
-      {children}
-    </main>
-  );
+  return <main className="flex flex-1 flex-col">{children}</main>;
 }
