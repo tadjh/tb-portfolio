81b1093f3d3239cead6d9f70dde236d86433a407
TadjhBrooks <tadjh.brooks@gmail.com>
2022-07-13 17:12:25 -0700

    Add dummy project data

--- config/projects.ts
+++ config/projects.ts
@@ -14,4 +14,16 @@ export const projects: Project[] = [
       "Sound effects and music recorded and edited in Ableton Live.",
     ],
   },
+  {
+    name: "Bingo Multiplayer React App",
+    image: bingoPic1,
+    hype: ["628 Hours", "10735 lines of codes"],
+    tasks: [
+      "Leveraged Socket.io to create an event-driven multiplayer room-based bingo game. Also available in singleplayer.",
+      "Server and client are coded in React/Typescript, architected in a yarn monorepo and deployed to Heroku.",
+      "The themeable React components were designed with TailwindCSS and tested in Storybook.",
+      "Multiplayer end-to-end tests were implemented in Cypress and thoroughly tested with Jest unit &#38; intergration tests.",
+      "Sound effects and music recorded and edited in Ableton Live.",
+    ],
+  },
 ];
