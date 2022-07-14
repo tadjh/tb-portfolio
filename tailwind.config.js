196df4a3fde79778e946d8579bd64670a8c1d462
TadjhBrooks <tadjh.brooks@gmail.com>
2022-07-13 19:37:55 -0700

    feat(env): Unify default transition duration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    transitionDuration: {
+      DEFAULT: "300ms",
+    },
     extend: {
       colors: {
         orange: {
