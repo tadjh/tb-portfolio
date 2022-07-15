e5cdf2b801032506cb03e56807f96bb5e7596fae
TadjhBrooks <tadjh.brooks@gmail.com>
2022-07-14 19:09:38 -0700

    fix: Change developer to engineer

--- config/constants.ts
+++ config/constants.ts
@@ -1,3 +1,3 @@
 export const TITLE_HOME = "Tadjh Brooks - Self-Taught Software Engineer";
 export const META_DESCRIPTION =
-  "Self-taught full-stack software developer with a voracious appetite for learning.";
+  "Self-taught full-stack software engineer with a voracious appetite for learning.";
