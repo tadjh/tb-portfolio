e507870232887c1ffb6e127b0c78c06e741be11e
tadjh <tadjh.brooks@gmail.com>
2024-06-04 18:15:33 -0700

    Remove useMemo

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,18 +1,16 @@
 import { faCopyright } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useMemo } from "react";
 import { config } from "../config";
 import CustomLink from "./CustomLink";
 
 export default function Copyright() {
-  const year = useMemo(() => new Date().getFullYear(), []);
   return (
     <div className="group relative flex flex-row items-center gap-x-2">
       <FontAwesomeIcon
         icon={faCopyright}
         className="h-4 w-4 transition-transform group-hover:scale-110"
       />
-      {year}
+      {new Date().getFullYear()}
       <CustomLink href="/" link>
         {config.url.vanity}
       </CustomLink>
