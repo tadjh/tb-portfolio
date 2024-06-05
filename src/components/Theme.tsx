ccbf3df62605b1d402efc53195629623d478bea0
tadjh <tadjh.brooks@gmail.com>
2024-06-04 22:50:06 -0700

    Fix client side themeing

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -2,10 +2,8 @@ import { useEffect, useState } from "react";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-type Themes = "light" | "dark" | undefined;
-
 export default function Theme() {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "dark");
 
   function handleClick() {
     setTheme(theme === "light" ? "dark" : "light");
@@ -23,14 +21,11 @@ export default function Theme() {
   const icon = theme === "light" ? faMoon : faSun;
 
   return (
-    <span
-      onClick={handleClick}
-      className="cursor-pointer transition-transform hover:scale-125"
-    >
+    <div onClick={handleClick} className="flex-1 cursor-pointer">
       <FontAwesomeIcon
         icon={icon}
-        className="h-4 w-10 text-black/90 dark:text-white/90"
+        className="h-4 w-10 text-black/90 transition-transform hover:scale-125 dark:text-white/90"
       />
-    </span>
+    </div>
   );
 }
