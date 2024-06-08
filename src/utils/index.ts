56a7e9432b8342d093723e30932509625471cc12
tadjh <tadjh.brooks@gmail.com>
2024-06-07 20:29:49 -0700

    Add Hero, Breadcrumbs, GridItem
    Update schema to simplify hero media

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,11 @@
 export function composeKey(label: string, index: number) {
   return `${label}-${index + 1}`;
 }
+
+export function isVideo(path: string) {
+  return /\.(mp4|webm)$/i.test(path);
+}
+
+export function isImage(path: string) {
+  return /\.(jpg|jpeg|png|gif|webp)$/i.test(path);
+}
