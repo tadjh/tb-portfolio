865c266d23eda5ac8fb8b5bba17314eb188a91d1
tadjh <tadjh.brooks@gmail.com>
2024-06-17 17:56:07 -0700

    Deprecate config

--- src/config/index.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  siteTitle: "Tadjh Brooks",
-  jobTitle: "Software Engineer",
-  email: "hello@tadjh.dev",
-  location: "Sherman Oaks, CA",
-  bio: "A full-stack software engineer with a voracious appetite for learning where I hope to apply my skills in developing, problem solving and user experience on real products that make a difference in people's lives.",
-  url: { domain: "https://tadjh.dev", vanity: "tadjh.dev" },
-};
