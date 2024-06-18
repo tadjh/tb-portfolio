743d97415c00becc47367bfe31b48307cf0bdaaf
tadjh <tadjh.brooks@gmail.com>
2024-06-17 17:53:55 -0700

    Change site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.tadjh.dev",
+  site: "https://www.tadjh.com",
   integrations: [tailwind(), react(), mdx(), sitemap()],
   prefetch: true,
   vite: {
