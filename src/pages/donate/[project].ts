501e7b5f66115aec84c3b8f9e37eba1b6ed01b66
tadjh <hello@tadjh.com>
2025-03-10 23:07:14 -0700

    Feat: donation routing

---
+++ src/pages/donate/[project].ts
@@ -0,0 +1,18 @@
+import type { APIRoute } from "astro";
+
+const projects = {
+  chatjack: "https://www.paypal.com/donate/?hosted_button_id=XHXZN8C8MZ2B6",
+};
+
+export const GET: APIRoute = ({ params, redirect }) => {
+  const project = params.project as keyof typeof projects;
+  if (project in projects) {
+    return redirect(projects[project], 307);
+  }
+  // If the project is not found, redirect to the main donation page
+  return redirect("https://tadjh.com");
+};
+
+export function getStaticPaths() {
+  return [{ params: { project: "chatjack" } }];
+}
